7f9b91204eff26391514566be2a78ceec9b3b0d8
JoaoPedroThomazTerossi <joaoptterossi@gmail.com>
2023-11-13 08:23:25 -0300

    COMEÇO

--- scripy.js
+++
@@ -1,5 +0,0 @@
-function cadastro() {
-    nome = document.getElementById("nmaluno").Value;
-    email = document.getElementById("emailaluno").Value;
-    document.getElementById("resposta").innerHTML = "Nome aluno" + nome + "<br> email: " + email;
-}
