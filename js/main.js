99855021f6b9838d1de0cf0241b36463f1deb00c
JoaoPedroThomazTerossi <joaoptterossi@gmail.com>
2023-12-03 13:54:02 -0300

    TESTE

--- js/main.js
+++ js/main.js
@@ -1,16 +1,7 @@
-/**
-* Template Name: Medilab
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+(function () {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
+
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -20,9 +11,7 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
+
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -34,16 +23,12 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
+
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -61,9 +46,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
+
   const scrollto = (el) => {
     let header = select('#header')
     let offset = header.offsetHeight
@@ -75,9 +58,7 @@
     })
   }
 
-  /**
-   * Toggle .header-scrolled class to #header when page is scrolled
-   */
+
   let selectHeader = select('#header')
   let selectTopbar = select('#topbar')
   if (selectHeader) {
@@ -98,9 +79,7 @@
     onscroll(document, headerScrolled)
   }
 
-  /**
-   * Back to top button
-   */
+
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
@@ -114,29 +93,23 @@
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
-  on('click', '.mobile-nav-toggle', function(e) {
+
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Mobile nav dropdowns activate
-   */
-  on('click', '.navbar .dropdown > a', function(e) {
+
+  on('click', '.navbar .dropdown > a', function (e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
       this.nextElementSibling.classList.toggle('dropdown-active')
     }
   }, true)
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
+
+  on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
 
@@ -151,9 +124,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
+
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -162,9 +133,7 @@
     }
   });
 
-  /**
-   * Preloader
-   */
+
   let preloader = select('#preloader');
   if (preloader) {
     window.addEventListener('load', () => {
@@ -172,23 +141,17 @@
     });
   }
 
-  /**
-   * Initiate glightbox 
-   */
+
   const glightbox = GLightbox({
     selector: '.glightbox'
   });
 
-  /**
-   * Initiate Gallery Lightbox 
-   */
+
   const galelryLightbox = GLightbox({
     selector: '.galelry-lightbox'
   });
 
-  /**
-   * Testimonials slider
-   */
+
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -215,9 +178,6 @@
     }
   });
 
-  /**
-   * Initiate Pure Counter 
-   */
   new PureCounter();
 
 })()
